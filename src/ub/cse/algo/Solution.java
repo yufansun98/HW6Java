c8abcb96c037a2c347f52b36c9f2775ac2e89a53
fan <yufansun@buffalo.edu>
2020-04-11 04:17:42 -0400

    done Dijkstra algorithm but may not correct

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -21,34 +21,37 @@ public class Solution {
         ArrayList<Integer> path = new ArrayList<>();
         ArrayList<Integer> s = new ArrayList<>();
         int base = 0;
-        Map<Integer,Integer>distance = new HashMap<>();
+        Integer[] distance = new Integer[graph.size()];
         for (int i = 0; i < graph.size(); i++){
-            distance.put(i, 2147483647);
+            distance[i] = 2147483647;
         }
         s.add(_startNode);
-        distance.put(_startNode, 0);
-        System.out.println(distance);
-        PriorityQueue<Integer>touched = new PriorityQueue<>(Comparator.comparingInt(node -> distance.get(node)));
+        distance[_startNode] = 0;
+        System.out.println(Arrays.toString(distance));
+        PriorityQueue<Integer>touched = new PriorityQueue<>(Comparator.comparingInt(node -> distance[node]));
         for (int i = 1; i < graph.get(_startNode).size(); i++){
+            distance[graph.get(_startNode).get(i)] =  graph.get(graph.get(_startNode).get(i)).get(0);
             touched.add(graph.get(_startNode).get(i));
-            distance.put(graph.get(_startNode).get(i), graph.get(graph.get(_startNode).get(i)).get(0));
         }
+        System.out.println(touched);
         while (touched.size() != 0){
             for (int i = 1; i < graph.get(touched.peek()).size(); i++){
                 int node = graph.get(touched.peek()).get(i);
-                int dis = base + graph.get(touched.peek()).get(0);
-                if(dis < distance.get(node)){
-                    distance.put(node, dis);
+                int dis = base + graph.get(graph.get(touched.peek()).get(i)).get(0);
+                if(dis < distance[node]){
+                    distance[node] = dis;
                     touched.add(node);
                 }
             }
-            base = distance.get(touched.peek());
+            base = distance[touched.peek()];
             s.add(touched.poll());
         }
+        System.out.println(base);
         if (!s.contains(_endNode)){
             return path;
         }
         else return s;
+
     }
 }
 
